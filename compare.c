ee32467fbd1d8a19c07b4620c19cfb9e3ef38595
lianyi-g <55772295+lianyi-g@users.noreply.github.com>
2021-01-27 23:41:59 +0800

    Add files via upload
    两整数比较大小

---
+++ compare.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+////----------------------3
+////两数比较大小采用函数调用
+//#include<stdio.h>
+//int compare(int x,int y)
+//{
+//	if(x>y)
+//		return x;
+//	else 
+//		return y;
+//}
+//
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int biggerOne = 0;
+//	printf("Please input two int num:\n");
+//	scanf("%d%d",&num1,&num2);
+//	biggerOne = compare(num1,num2);
+//	printf("The biggerOne is :%d\n",biggerOne);
+//	return 0;
+//}
+//----------------------2
+
+////两数比较大小
+//#include<stdio.h>
+//int main()
+//{
+//	int num1 = 20;
+//	int num2 = 30;
+//	int biggerOne =0;
+//	if(num1>num2)
+//		biggerOne = num1;
+//	else
+//		biggerOne = num2;
+//	printf("The biggerOne is %d\n.",biggerOne);
+//	return 0;
+//}
+
+
+//----------------------1
+//#include<stdio.h>
+////两数相加
+//int main()
+//{
+//	int num1 = 20;
+//	int num2 = 30;
+//	int sum =0;
+//	sum = num1 + num2;
+//	printf("The sum is %d\n.",sum);
+//	return 0;
+//}
