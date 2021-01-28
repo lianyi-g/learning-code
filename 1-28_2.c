3a48c8066197e5d2d0ed422ddfced4e6febdb9df
lianyi-g <55772295+lianyi-g@users.noreply.github.com>
2021-01-28 22:22:24 +0800

    Add files via upload

---
+++ 1-28_2.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+////------------3
+//#include<stdio.h>
+//int main()
+//{
+//	//当条件判断时，若为变量和常量做比，那么常量放左边，可以防止出现 赋值型判断bug
+//	int a = 5;
+//	int num = 0;
+//	printf("Please input num :");
+//	scanf("%d",&num);
+//	if(6 == num)
+//		printf("你刚刚输入的是5+1 = %d\n",num);
+//	return 0;
+//}
+//#include<stdio.h>
+////------------2
+//int main()
+//{
+//	int a = 0;
+//	int b = 2;
+//	if(a ==1)
+//	{
+//		if(b==2)
+//			printf("hello\n");
+//	}
+//		else
+//			printf("bye\n");
+//	return 0;
+//}
+//------------1
+//int main()
+//{
+//	int age = 999;
+//	if(age<15)
+//		printf("未成年！\n");
+//	else if(age>=15 && age<30)
+//		printf("青年！\n");
+//	else if(age>=30 && age<50)
+//		printf("壮年！\n");
+//	else if(age>=50 && age<90)
+//		printf("老年！\n");
+//	else 
+//		printf("神仙486！\n");
+//	return 0;
+//}
