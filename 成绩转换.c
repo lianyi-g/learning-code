d185e8f54c9e1fe840355b7dbd53df41fb9aedb5
lianyi-g <55772295+lianyi-g@users.noreply.github.com>
2021-02-17 23:51:00 +0800

    Add files via upload

---
+++ 成绩转换.c
@@ -0,0 +1,38 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int score;
+//	while(scanf("%d",&score)!=EOF)
+//	{
+//		getchar();
+//		if(score > 100 || score < 0)
+//		{
+//			printf("Score is error!\n");
+//			continue;
+//		}
+//		score = score / 10;
+//		switch(score)
+//		{
+//		case 10:
+//		case 9:
+//			printf("A\n");
+//			break;
+//		case 8:
+//			printf("B\n");
+//			break;
+//		case 7:
+//			printf("C\n");
+//			break;
+//		case 6:
+//			printf("D\n");
+//			break;
+//		default:
+//			printf("E\n");
+//			break;
+//		}
+//	}
+//	return 0;
+//}
