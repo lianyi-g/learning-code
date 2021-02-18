ca4198e1dd76f829168fc5cce429ad822f495e57
lianyi-g <55772295+lianyi-g@users.noreply.github.com>
2021-02-18 20:26:34 +0800

    Add files via upload
    2021-2-18学习代码

---
+++ 奇数乘积.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n,num,ret;
+//	while(scanf("%d",&n)!=EOF)
+//	{
+//		getchar();
+//		ret = 1;
+//		for(; n>0; n--)
+//		{
+//			scanf("%d",&num);
+//			if(num%2!=0)
+//				ret *= num;
+//		}
+//		printf("%d\n",ret);
+//	}
+//	return 0;
+//}
