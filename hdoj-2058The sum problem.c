3fc31a75a69a016352f909876b9d0d2a934ba445
lianyi-g <55772295+lianyi-g@users.noreply.github.com>
2021-03-12 13:05:25 +0800

    Add files via upload
    2021-3-11学习代码

---
+++ hdoj-2058The sum problem.c
@@ -0,0 +1,29 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+////error
+//int main()
+//{
+//	int n,m,sum,start,i,j;
+//	while(scanf("%d%d",&n,&m)!=EOF && n+m)
+//	{
+//		for(i=1; i<=m/2&&i<=n; i++)
+//		{
+//			sum = 0;
+//			start = i;
+//			for(j=i; j<=m/2+1&&j<=n; j++)
+//			{
+//				sum += j;
+//				if(sum==m)
+//				{
+//					printf("[%d,%d]\n",start,j);
+//					break;
+//				}
+//			}
+//		}
+//		if(n>=m)
+//			printf("[%d,%d]\n",m,m);
+//		printf("\n");
+//	}
+//	return 0;
+//}
