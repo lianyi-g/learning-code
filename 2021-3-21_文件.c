d29ffdf2f2c8afbffd5366a0e992c0ecd18a93c6
lianyi-g <55772295+lianyi-g@users.noreply.github.com>
2021-03-22 23:10:02 +0800

    Add files via upload
    2021-3-21学习代码

---
+++ 2021-3-21_文件.c
@@ -0,0 +1,13 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 10000;
+//	FILE* pf = fopen("test.txt","wb");
+//	fwrite(&a,4,1,pf);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
---
+++ hdoj-2075A-B.c
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int t,a,b;
+//	scanf("%d",&t);
+//	while(t-- && scanf("%d%d",&a,&b))
+//	{
+//		if(a%b==0)
+//			printf("YES\n");
+//		else
+//			printf("NO\n");
+//	}
+//	return 0;
+//}
