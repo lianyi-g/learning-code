44a46e6dc39f58fe8c9cafb53884ea3ccdeced77
lianyi-g <55772295+lianyi-g@users.noreply.github.com>
2021-03-23 18:35:33 +0800

    Add files via upload
    2021-3-22学习代码

---
+++ 2021-3-22_test.c
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <math.h>
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int n;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		if(n<=35 && n>=1)
+//			printf("%.0f\n", pow(3, n) - 1);
+//	}
+//		
+//	return 0;
+//}
