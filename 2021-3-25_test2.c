0c4a19abbcdea46df352c3cc4661ff0031c56216
lianyi-g <55772295+lianyi-g@users.noreply.github.com>
2021-03-26 09:28:01 +0800

    Add files via upload
    2021-3-25学习代码

---
+++ 2021-3-25_test2.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	double n;
+//	while(scanf("%lf",&n)!=EOF)
+//	{
+//		printf("%.2f\n",fabs(n));
+//		//if(n<0)
+//		//	printf("%.2f\n",-n);
+//		//else
+//		//	printf("%.2f\n",n);
+//	}
+//	return 0;
+//}
